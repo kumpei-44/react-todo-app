f3236336da6b04994b1b0cf552973873bb30c0f5
kumpei hayashi <km.abs104@gmail.com>
2020-04-27 20:45:47 +0900

    Material-UI追加

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,20 +1,39 @@
 import React from 'react';
+import Reboot from 'material-ui/Reboot';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typograpphy from 'material-ui/Typography';
+import Button from 'material-ui/Button';
+import Input from 'material-ui/Input';
+import List, {ListItem, ListItemText} from 'material-ui/List';
 
 export default function TodoApp({task, tasks, addTask, inputTask, redirectToError}) {
   return (
     <div>
-      <input type="text" onChange={(e) => inputTask(e.target.value)} />
-      <input type="button" value="add" onClick={() => addTask(task)} />
-      <ul>
-        {
-          tasks.map(function(item, i) {
-            return (
-              <li key={i}>{item}</li>
-            );
-          })
-        }
-      </ul>
-      <button onClick={() => redirectToError()} >エラーページへ</button>
+      <Reboot />
+      <AppBar position="static">
+        <Toolbar>
+          <Typograpphy type="title" color="inherit">
+            Todo
+          </Typograpphy>
+        </Toolbar>
+      </AppBar>
+      <div style={{padding: '16px'}}>
+        <Input onChange={(e) => inputTask(e.target.value)} />
+        <Button raised color="secondary" onClick={() => addTask(task)}>add</Button>
+        <List>
+          {
+            tasks.map(function(item, i) {
+              return (
+                <ListItem key={i}>
+                  <ListItemText primary={`・${item}`} />
+                </ListItem>
+              );
+            })
+          }
+        </List>
+        <Button raised color="access" onClick={() => redirectToError()} >エラーページへ</Button>
+      </div>
     </div>
   );
 }
